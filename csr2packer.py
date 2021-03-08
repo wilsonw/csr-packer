3e8117d144c1509b0616d019edbf53eac0fa1baa
Wilson Wu <w@wmnlfamily.com>
2021-03-08 15:14:52 +0800

    added pack function

--- csr2packer.py
+++ csr2packer.py
@@ -1,6 +1,7 @@
 from collections import OrderedDict
 import gzip
 import json
+import logging
 import os
 
 ONLINE_KEY = "UDMZr24F" # not sure when this is used but csrpacker always use offline key
@@ -8,7 +9,12 @@ OFFLINE_KEY = "4cPw3ZyC"
 
 VALID_FILE_NAMES = ("nsb", "scb", "trb")
 
+logger = logging.getLogger(__name__)
+
 def unpackFiles(path="", outputPath=""):
+    if not outputPath:
+        outputPath = os.path.join(path, "unpacked")
+    os.makedirs(outputPath, exist_ok=True)
     for filename in VALID_FILE_NAMES:
         try:
             with gzip.open(os.path.join(path, filename), 'rb') as f:
@@ -18,18 +24,41 @@ def unpackFiles(path="", outputPath=""):
                     content = content[:-1]
 
                 data = json.loads(content, object_pairs_hook=OrderedDict)
-                
-                if not outputPath:
-                    outputPath = os.path.join(path, "unpacked")
                 outputFileName = os.path.join(outputPath, filename + '.txt')
-                os.makedirs(outputPath, exist_ok=True)
                 
                 try:
                     with open(outputFileName, 'w+') as outputFile:
                         json.dump(data, outputFile, indent=4, sort_keys=False)
-                        print('Converted {}'.format(filename))
+                        logger.info('Converted %s', filename)
                 except:
-                    print("Failed to generate output {}".format(outputFileName))
+                    logger.error("Failed to generate output %s", outputFileName, exc_info=True)
         except (IOError, FileNotFoundError):
-            print("Ignore {}, not found".format(filename))
+            logger.info("Ignore %s, not found", filename)
             
+def packFiles(path="", outputPath=""):
+    import hmac
+    import binascii
+
+    if not outputPath:
+        outputPath = os.path.join(path, "packed")
+    os.makedirs(outputPath, exist_ok=True)
+    for filename in VALID_FILE_NAMES:
+        try:
+            with open(os.path.join(path, filename + '.txt'), 'r') as f:
+                data = json.load(f, object_pairs_hook=OrderedDict)
+                minimizedString = json.dumps(data, separators=(',', ':'))
+                hashString = hmac.digest(bytes(OFFLINE_KEY, 'utf-8'), bytes(minimizedString, 'utf-8'), 'sha1')
+                hashString = binascii.hexlify(hashString)
+                logger.debug("Hash for %s is %s", filename, hashString)
+            try:
+                with gzip.open(os.path.join(outputPath, filename), 'w+b') as outputFile:
+                    outputFile.write(hashString)
+                    outputFile.write(b'\n')
+                    outputFile.write((bytes(minimizedString, 'utf-8')))
+                    outputFile.write(b'\0')
+                    logger.info('Generated %s', filename)
+            except:
+                logger.error("failed to generate output %s", filename, exc_info=True)
+        except (IOError, FileNotFoundError):
+            logger.info("Ignore %s.txt, not found", filename)
+
